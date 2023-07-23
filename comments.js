6dd90492aa87680a6a2bd992922b3b9f53e0d2a9
BjoernAuxx <140315152+BjoernAuxx@users.noreply.github.com>
2023-07-23 14:54:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,26 @@
-// create web server
+//create web server
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+//load comment model
+const Comment = require('../models/comment');
+const config = require('../config/database');
+
+//add comment
+router.post('/add', (req, res, next) => {
+    let newComment = new Comment({
+        text: req.body.text,
+        user: req.body.user,
+        post: req.body.post
+    });
+    
+    Comment.addComment(newComment, (err, comment) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to add comment'});
+        } else {
+            res.json({success: true, msg: 'Comment added'});
+        }
+    });
+}   );
